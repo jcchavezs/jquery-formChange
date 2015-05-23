05f6523163eba9d8d93ee7af2f05bd409b1af10e
José Carlos Chávez <jcchavezs@gmail.com>
2015-05-23 13:44:54 +0200

    Refactored the plugin for using events instead of callback functions.

--- jquery.formChange.js
+++ jquery.formChange.js
@@ -1,52 +1,45 @@
-/*! jquery-formChange - v0.0.1 (2013-12-14)
+/*! jquery-formChange - v1.0.0 (2015-05-23)
 * -----------------
 * Copyright(c) 2013 José Carlos Chávez <jcchavezs@gmail.com>
 */
 
 (function($) {
     $.fn.formChange = function(options) {
-        this.each(function() {
-            var form = this;
+        options = $.extend({}, $.fn.formChange.defaults, options);
 
-            $(form).on('submit', function(){
-                options.callback(false, form);
+        this.each(function() {
+            var $form = $(this);
+
+            $form.on('submit', function(){
+
+                $form.trigger('form-change', [ false ]);
+
             }).find('input, select, textarea').each(function(i, e) {
+
                 $(e).data('initial-value', $(e).val()).on('change', function() {
                     if ($(e).val() === $(e).data('initial-value')) {
-                        if (options.hasOwnProperty('class')) {
-                            $(e).removeClass(options.class);
+                        if (options.hasOwnProperty('hasChangedClass')) {
+                            $(e).removeClass(options.hasChangedClass);
                         }
 
                         $(e).removeAttr('data-form-change');
                     } else {
-                        if (options.hasOwnProperty('class')) {
-                            $(e).addClass(options.class);
+                        if (options.hasOwnProperty('hasChangedClass')) {
+                            $(e).addClass(options.hasChangedClass);
                         }
 
                         $(e).attr('data-form-change', true);
                     }
 
-                    if ($(form).find('[data-form-change]').length === 0) {
-                        switch (typeof options.callback) {
-                            case 'string':
-                                window[options.callback](false, form);
-                                break;
-                            case 'function':
-                                options.callback(false, form);
-                                break;
-                        }
-                    } else {
-                        switch (typeof options.callback) {
-                            case 'string':
-                                window[options.callback](true, form);
-                                break;
-                            case 'function':
-                                options.callback(true, form);
-                                break;
-                        }
-                    }
+                    $form.trigger('form-change', [ ($form.find('[data-form-change]').length !== 0) ]);
                 });
+
             });
         });
     };
+
+    $.fn.formChange.defaults = {
+        hasChangedClass: 'has-changed'
+    };
+
 })(jQuery);
